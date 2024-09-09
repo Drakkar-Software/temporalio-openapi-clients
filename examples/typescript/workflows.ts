f545e501d050402b4fb4c2b1b1ab2de58bce46f3
Herklos <herklos@drakkar.software>
2024-09-09 23:15:13 +0200

    Initial commit
    Signed-off-by: Herklos <herklos@drakkar.software>

--- examples/typescript/workflows.ts
+++ examples/typescript/workflows.ts
@@ -3,7 +3,7 @@ import { TemporalIOAPIClient } from "../../client/typescript/index";
 async function fetchWorkflows() {
   const client = new TemporalIOAPIClient({
     BASE: process.env.TEMPORAL_API_URL,
-    HEADERS: { "Authorization": `Bearer ${process.env.TEMPORAL_API_KEY}` } // https://pennylane.readme.io/reference/company-api-key
+    HEADERS: { "Authorization": `Bearer ${process.env.TEMPORAL_API_KEY}` }
   });
 
   const workflows = await client.workflowService.listWorkflowExecutions(process.env.TEMPORAL_NAMESPACE || '');
